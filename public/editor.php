d05421a132220d4ddf0a3fc9fbe52eec72d6a915
asherShores5 <71547146+asherShores5@users.noreply.github.com>
2023-04-04 20:13:29 -0700

    fetch update

--- public/editor.php
+++ public/editor.php
@@ -1,6 +1,6 @@
 <?php
-// header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
 
 // $code = $_POST["code"];
 // $filename = "../src/input.artsy"; // Change this to the name of the file you want to save to
