4f6b64e44210d2b6efe87534c9a3f18a9d5f7ec5
asherShores5 <71547146+asherShores5@users.noreply.github.com>
2023-04-04 19:41:54 -0700

    try again pls work

--- public/editor.php
+++ public/editor.php
@@ -1,8 +1,20 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Methods: POST");
 
-$code = $_POST["code"];
-$filename = "../src/input.artsy"; // Change this to the name of the file you want to save to
-file_put_contents($filename, $code);
+// $code = $_POST["code"];
+// $filename = "../src/input.artsy"; // Change this to the name of the file you want to save to
+// file_put_contents($filename, $code);
+
+  // Get the code from the POST request
+  $code = $_POST["code"];
+
+  // Open the file for writing (this will create a new file if it doesn't exist)
+  $file = fopen("path/to/your/file.js", "w");
+
+  // Write the code to the file
+  fwrite($file, $code);
+
+  // Close the file
+  fclose($file);
 ?>
