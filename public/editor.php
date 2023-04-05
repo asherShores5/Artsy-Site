68c1f3c5cf313e9ba3d6a95431ade94275d949f0
asherShores5 <71547146+asherShores5@users.noreply.github.com>
2023-04-04 20:28:58 -0700

    pls

--- public/editor.php
+++ public/editor.php
@@ -1,20 +1,13 @@
 <?php
+//echo 'PHP File Started';
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 
-// $code = $_POST["code"];
-// $filename = "../src/input.artsy"; // Change this to the name of the file you want to save to
+$code = $_POST["code"];
+$filename = "../src/input.artsy"; // Change this to the name of the file you want to save to
 // file_put_contents($filename, $code);
 
-  // Get the code from the POST request
-  $code = $_POST["code"];
-
-  // Open the file for writing (this will create a new file if it doesn't exist)
-  $file = fopen("../src/input.artsy", "w");
-
-  // Write the code to the file
-  fwrite($file, $code);
-
-  // Close the file
-  fclose($file);
+$file = fopen($filename, "w");
+fwrite($file, $code);
+fclose($file);
 ?>
