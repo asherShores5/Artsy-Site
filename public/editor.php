646961de8a52d6bb15d8f339d0bd80c6e91d34d5
asherShores5 <71547146+asherShores5@users.noreply.github.com>
2023-04-04 19:04:16 -0700

    AJAX web security i guess

--- public/editor.php
+++ public/editor.php
@@ -1,11 +1,8 @@
 <?php
-// $file = 'example-programs/run.artsy';
-// $textToWrite = $_POST['textToWrite'];
-
-// file_put_contents($file, $textToWrite);
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
 
 $code = $_POST["code"];
 $filename = "../src/input.artsy"; // Change this to the name of the file you want to save to
 file_put_contents($filename, $code);
-
 ?>
