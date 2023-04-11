26ccc574271ab8e3f4c6aa1003f5c7cea38cf58a
asherShores5 <71547146+asherShores5@users.noreply.github.com>
2023-04-10 17:49:10 -0700

    composer sucks

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0bc897e6d830137769852ec7ad2efcc33842d4f5',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0bc897e6d830137769852ec7ad2efcc33842d4f5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'php-memcached/php-memcached' => array(
+            'pretty_version' => '3.1.5',
+            'version' => '3.1.5.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../php-memcached/php-memcached',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
