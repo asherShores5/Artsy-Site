79dd95a75b5f710593233e29032c954b262d1398
drewesch <wheels.esch@gmail.com>
2023-04-15 01:31:15 -0700

    New Deployment Script

--- .azure/deploy.sh
+++ .azure/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Update package list and install dependencies
-sudo apt-get update
-sudo apt-get install -y make flex bison wabt build-essential gcc
+apt update
+apt install -y make flex bison wabt build-essential gcc
 
 # Change permissions for the specified directory
-sudo chown -R www-data:www-data /home/site/wwwroot/src
-sudo chmod -R 755 /home/site/wwwroot/src
+chown -R www-data:www-data /home/site/wwwroot/src
+chmod -R 755 /home/site/wwwroot/src
