82ea1cd21ba732e38e8e01baca1f185252fe10ac
drewesch <wheels.esch@gmail.com>
2023-04-15 11:43:46 -0700

    New deployment method

--- scripts/postdeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Update package list and install dependencies
-sudo apt update
-sudo apt install -y make flex bison wabt build-essential gcc
-
-# Change permissions for the specified directory
-sudo chown -R www-data:www-data /home/site/wwwroot/src
-sudo chmod -R 755 /home/site/wwwroot/src
