0b836a3c32034a99957a84e0371971fc5e54d99a
drewesch <wheels.esch@gmail.com>
2023-04-17 20:36:46 -0700

    Initial URL rewriting support (commented for now)

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
     session_start();
+    // header("Cache-Control: no-cache, must-revalidate");    
     header('Location: public/index.html');
     phpinfo();
     exit;
